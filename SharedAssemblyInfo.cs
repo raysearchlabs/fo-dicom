465c2db1b83465849ac0ab0422bf591b1be51e16
Rickard Holmberg <rickard.holmberg@raysearchlabs.com>
2016-09-07 10:11:05 +0200

    Bump version to 1.0.56

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -10,6 +10,6 @@ using System.Reflection;
 [assembly: AssemblyTrademarkAttribute("")]
 [assembly: AssemblyCultureAttribute("")]
 
-[assembly: AssemblyVersionAttribute("1.0.55.0")]
-[assembly: AssemblyFileVersion("1.0.55.0")]
+[assembly: AssemblyVersionAttribute("1.0.56.0")]
+[assembly: AssemblyFileVersion("1.0.56.0")]
 [assembly: AssemblyInformationalVersionAttribute("1.0.3")]
