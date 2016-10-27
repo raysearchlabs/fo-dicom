83cd486f63b8b4b4187a4583b86153346be7fc2f
Rickard Holmberg <rickard.holmberg@raysearchlabs.com>
2016-10-27 13:28:00 +0200

    Added DicomResponseTest

---
+++ DICOM [Unit Tests]/Network/DicomResponseTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dicom;
+using Dicom.Network;
+using Xunit;
+
+namespace DICOM__Unit_Tests_.Network
+{
+    public class DicomResponseTest
+    {
+        [Fact]
+        public void ErrorCommentIsNotThereForSuccess()
+        {
+            var rsp = new DicomResponse(new DicomDataset());
+
+            rsp.Status = DicomStatus.Success;
+
+            Assert.False(rsp.Command.Contains(DicomTag.ErrorComment));
+        }
+
+        [Fact]
+        public void ErrorCommentIsThereForFailure()
+        {
+            var rsp = new DicomResponse(new DicomDataset());
+
+            var failure = new DicomStatus("FF01", DicomState.Failure, "Failed", "Comment on the failure mode");
+
+            rsp.Status = failure;
+
+            Assert.True(rsp.Command.Contains(DicomTag.ErrorComment));
+        }
+    }
+}
